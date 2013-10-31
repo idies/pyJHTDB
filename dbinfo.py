3d6fc206ff43a41949172a30adac0f40aa040ba1
Chichi Lalescu <clalesc1@jhu.edu>
2013-10-31 11:41:36 -0400

    add dbinfo dictionaries

---
+++ dbinfo.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+isotropic1024coarse = {'name': 'isotropic1024coarse',
+                       'nx'  : 1024,
+                       'ny'  : 1024,
+                       'nz'  : 1024,
+                       'lx'  : 2*np.pi,
+                       'ly'  : 2*np.pi,
+                       'lz'  : 2*np.pi,
+                       'dx'  : np.pi/512,
+                       'dy'  : np.pi/512,
+                       'dz'  : np.pi/512}
