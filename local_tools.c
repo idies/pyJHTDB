c527393a2b42a2d63745d99f75a94033c5f83df1
Chichi Lalescu <clalesc1@jhu.edu>
2014-06-26 10:11:26 -0400

    add bounded B line solver

--- local_tools.c
+++ local_tools.c
@@ -1,8 +1,16 @@
+/*  
+ *  These subroutines are meant to be called from a pyJHTDB.libTDB object.
+ *  You're on your own otherwise.
+ *
+ * */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 #include "turblib.h"
+
+float float_error = 1e-10;
 
 int getBline(
         char *authToken,
@@ -35,7 +43,7 @@ int getBline(
             y[p][1] = x[p][1] + ds*bfield0[p][1];
             y[p][2] = x[p][2] + ds*bfield0[p][2];
         }
-        // get bhat1 and compute xnext
+        // get bhat1 and compute next position
         getMagneticField(authToken, dataset, time, spatial, temporal, count, y, bfield1);
         for (p = 0; p < count; p++)
         {
@@ -55,4 +63,132 @@ int getBline(
     return 0;
 }
 
+int getRectangularBoundedBline(
+        char *authToken,
+        char *dataset,
+        float time,
+        int maxsteps,
+        float ds,
+        enum SpatialInterpolation spatial,
+        enum TemporalInterpolation temporal,
+        int count,
+        float traj[][3],
+        int traj_length[],
+        float xmin, float xmax,
+        float ymin, float ymax,
+        float zmin, float zmax)
+{
+    // traj will contain all the trajectories, and traj_length will contain the lengths of all trajectories
+    // the shape of traj is assumed to be (number of particles, allocated_traj_length, 3)
+    int p, s;
+    float bsize;
+    float (*x)[3];
+	float y[1][3];
+	float bfield0[1][3];
+	float bfield1[1][3];
+
+    //loop after particles
+    for (p = 0; p < count; p++)
+    {
+        // x is at the start of the current trajectory
+        x = traj + p*(maxsteps+1);
+        for (s = 1; s <= maxsteps; s++)
+        {
+            // get bhat0 and compute y
+            getMagneticField(authToken, dataset, time, spatial, temporal, 1, x, bfield0);
+            bsize = sqrt(bfield0[0][0]*bfield0[0][0]
+                       + bfield0[0][1]*bfield0[0][1]
+                       + bfield0[0][2]*bfield0[0][2]);
+            bfield0[0][0] /= bsize;
+            bfield0[0][1] /= bsize;
+            bfield0[0][2] /= bsize;
+            y[0][0] = x[0][0] + ds*bfield0[0][0];
+            y[0][1] = x[0][1] + ds*bfield0[0][1];
+            y[0][2] = x[0][2] + ds*bfield0[0][2];
+            // get bhat1 and compute next position
+            getMagneticField(authToken, dataset, time, spatial, temporal, 1, y, bfield1);
+            bsize = sqrt(bfield1[0][0]*bfield1[0][0]
+                       + bfield1[0][1]*bfield1[0][1]
+                       + bfield1[0][2]*bfield1[0][2]);
+            bfield1[0][0] /= bsize;
+            bfield1[0][1] /= bsize;
+            bfield1[0][2] /= bsize;
+            x[1][0] = x[0][0] + .5*ds*(bfield0[0][0] + bfield1[0][0]);
+            x[1][1] = x[0][1] + .5*ds*(bfield0[0][1] + bfield1[0][1]);
+            x[1][2] = x[0][2] + .5*ds*(bfield0[0][2] + bfield1[0][2]);
+            x += 1;
+            if (((*x)[0] < xmin || (*x)[0] > xmax)
+             || ((*x)[1] < ymin || (*x)[1] > ymax)
+             || ((*x)[2] < zmin || (*x)[2] > zmax))
+                break;
+        }
+        traj_length[p] = s+1;
+    }
+    return 0;
+}
+
+int getSphericalBoundedBline(
+        char *authToken,
+        char *dataset,
+        float time,
+        int maxsteps,
+        float ds,
+        enum SpatialInterpolation spatial,
+        enum TemporalInterpolation temporal,
+        int count,
+        float traj[][3],
+        int traj_length[],
+        float ox,
+        float oy,
+        float oz,
+        float radius)
+{
+    // traj will contain all the trajectories, and traj_length will contain the lengths of all trajectories
+    // the shape of traj is assumed to be (number of particles, allocated_traj_length, 3)
+    int p, s;
+    float bsize;
+    float (*x)[3];
+	float y[1][3];
+	float bfield0[1][3];
+	float bfield1[1][3];
+
+    //loop after particles
+    for (p = 0; p < count; p++)
+    {
+        // x is at the start of the current trajectory
+        x = traj + p*(maxsteps+1);
+        for (s = 1; s <= maxsteps; s++)
+        {
+            // get bhat0 and compute y
+            getMagneticField(authToken, dataset, time, spatial, temporal, 1, x, bfield0);
+            bsize = sqrt(bfield0[0][0]*bfield0[0][0]
+                       + bfield0[0][1]*bfield0[0][1]
+                       + bfield0[0][2]*bfield0[0][2]);
+            bfield0[0][0] /= bsize;
+            bfield0[0][1] /= bsize;
+            bfield0[0][2] /= bsize;
+            y[0][0] = x[0][0] + ds*bfield0[0][0];
+            y[0][1] = x[0][1] + ds*bfield0[0][1];
+            y[0][2] = x[0][2] + ds*bfield0[0][2];
+            // get bhat1 and compute next position
+            getMagneticField(authToken, dataset, time, spatial, temporal, 1, y, bfield1);
+            bsize = sqrt(bfield1[0][0]*bfield1[0][0]
+                       + bfield1[0][1]*bfield1[0][1]
+                       + bfield1[0][2]*bfield1[0][2]);
+            bfield1[0][0] /= bsize;
+            bfield1[0][1] /= bsize;
+            bfield1[0][2] /= bsize;
+            x[1][0] = x[0][0] + .5*ds*(bfield0[0][0] + bfield1[0][0]);
+            x[1][1] = x[0][1] + .5*ds*(bfield0[0][1] + bfield1[0][1]);
+            x[1][2] = x[0][2] + .5*ds*(bfield0[0][2] + bfield1[0][2]);
+            x += 1;
+            if (sqrt((x[0][0] - ox)*(x[0][0] - ox)
+                   + (x[0][1] - oy)*(x[0][1] - oy)
+                   + (x[0][2] - oz)*(x[0][2] - oz)) > radius + float_error)
+                break;
+        }
+        traj_length[p] = s+1;
+    }
+    return 0;
+}
 
