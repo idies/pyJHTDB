9ecd82ff17c069055bc4aaf08419592becf5a2c8
Chichi Lalescu <clalesc1@jhu.edu>
2014-09-16 10:10:49 -0400

    make test consistent with new get_big_cutout

--- test.py
+++ test.py
@@ -29,13 +29,15 @@ def clean_2D_field(
 
 def main():
     pyJHTDB.cutout.get_big_cutout(
-            cube_dim = 32,
+            t0 = 0, tl = 2,
+            x0 = 0, xl = 48,
+            y0 = 0, yl = 16,
+            z0 = 0, zl = 32,
             chunk_dim = 16,
-            data_type = 'u',
+            data_type = 'ub',
             filename = 'tmp',
-            base_website = 'test.turbulence.pha.jhu.edu')
+            base_website = 'turbulence.pha.jhu.edu')
     data = h5py.File('tmp.h5', mode = 'r')
-    print data['u00000'].shape
     energy = (data['u00000'][0, :, :, 0]**2
             + data['u00000'][0, :, :, 1]**2
             + data['u00000'][0, :, :, 2]**2)
