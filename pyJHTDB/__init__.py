2988357bcdc6a3a5525b2ccf5c1fd354ec2ead76
Chichi Lalescu <clalesc1@jhu.edu>
2014-10-12 17:59:31 -0400

    only import cutout if h5py present

--- pyJHTDB/__init__.py
+++ pyJHTDB/__init__.py
@@ -78,8 +78,11 @@ if os.path.isfile(homefolder + '/JHTDB_user_token.txt'):
 try:
     import h5py
 except ImportError:
+    h5py = None
     print 'h5py not found. cutout functionality not available.'
 
 from libJHTDB import *
-from test import test_plain, test_misc, test_cutout
+from test import test_plain, test_misc
+if h5py:
+    from test import test_cutout
 
