73e49e6c1ac93c36b295c629bcd9375ac2c16ee5
Chichi Lalescu <clalesc1@jhu.edu>
2014-11-23 23:20:49 -0500

    interpolator: use component-wise options

--- pyJHTDB/interpolator.py
+++ pyJHTDB/interpolator.py
@@ -43,13 +43,27 @@ class spline_interpolator:
             info,
             n = 1,
             m = 1,
-            compute_fast_beta = False):
-        self.n = n
-        self.m = m
+            compute_fast_beta = False,
+            nx = None,
+            mx = None,
+            ny = None,
+            my = None,
+            nz = None,
+            mz = None):
+        self.nx = n if (type(nx) == type(None)) else nx
+        self.ny = n if (type(ny) == type(None)) else ny
+        self.nz = n if (type(nz) == type(None)) else nz
+        self.mx = m if (type(mx) == type(None)) else mx
+        self.my = m if (type(my) == type(None)) else my
+        self.mz = m if (type(mz) == type(None)) else mz
         self.info = info
         pickle_file = os.path.join(
                 pyJHTDB.lib_folder,
-                info['name'] + '_spline_interpolator_n{0}_m{1}.pickle.gz'.format(n, m))
+                info['name'] + '_spline_interpolator_' +
+                'xn{0}m{1}_'.format(self.nx, self.mx) +
+                'yn{0}m{1}_'.format(self.ny, self.my) +
+                'zn{0}m{1}.'.format(self.nz, self.mz) +
+                'pickle.gz')
         if os.path.exists(pickle_file):
             self.spline = pickle.load(gzip.open(pickle_file))
         else:
@@ -58,14 +72,14 @@ class spline_interpolator:
                 if info[coord + 'uniform'] and info[coord + 'periodic']:
                     func.append(gs.generic_spline_1D(
                             info[coord + 'nodes'][:2],
-                            max_deriv = m,
-                            neighbours = n,
+                            max_deriv = getattr(self, 'm' + coord),
+                            neighbours = getattr(self, 'n' + coord),
                             period = info['l' + coord]))
                 else:
                     func.append(gs.generic_spline_1D(
                             info[coord + 'nodes'],
-                            max_deriv = m,
-                            neighbours = n))
+                            max_deriv = getattr(self, 'm' + coord),
+                            neighbours = getattr(self, 'n' + coord)))
                 func[-1].compute_derivs()
                 func[-1].compute_beta()
             self.spline = {'x': func[0],
@@ -95,35 +109,35 @@ class spline_interpolator:
             getFunction = 'getVelocityAndPressure'):
         if (not len(points.shape) == 2):
             return None
-        field_points = np.zeros((points.shape[0], 2*self.n+2, 2*self.n+2, 2*self.n+2, 3), dtype = np.float32)
+        field_points = np.zeros((points.shape[0], 2*self.nz+2, 2*self.ny+2, 2*self.nx+2, 3), dtype = np.float32)
         xgrid = np.floor(points[:, 0] / self.info['dx']).astype(np.int) % self.info['nx']
         xfrac = (points[:, 0] - self.info['xnodes'][xgrid])/self.info['dx']
         for p in range(points.shape[0]):
             field_points[p, :, :, :, 0] = (self.info['xnodes'][xgrid[p]]
-                    + np.array(list(range(-self.n, self.n+2)))*self.info['dx'])[np.newaxis, np.newaxis, :]
+                    + np.array(list(range(-self.nx, self.nx+2)))*self.info['dx'])[np.newaxis, np.newaxis, :]
         if self.info['yperiodic']:
             ygrid = np.floor(points[:, 1] / self.info['dy']).astype(np.int) % self.info['ny']
             yfrac = (points[:, 1] - self.info['ynodes'][ygrid])/self.info['dy']
             for p in range(points.shape[0]):
                 field_points[p, :, :, :, 1] = (self.info['ynodes'][ygrid[p]]
-                        + np.array(list(range(-self.n, self.n+2)))*self.info['dy'])[np.newaxis, :, np.newaxis]
+                        + np.array(list(range(-self.ny, self.ny+2)))*self.info['dy'])[np.newaxis, :, np.newaxis]
         else:
             ygrid = np.searchsorted(self.info['ynodes'], points[:, 1]).astype(np.int) - 1
             yfrac = (points[:, 1] - self.info['ynodes'][ygrid])/self.info['dy'][ygrid]
             for p in range(points.shape[0]):
                 if ygrid[p] < 0 or ygrid[p] > self.info['ny'] - 1:
                     return None
-                elif ygrid[p] < self.n:
-                    field_points[p, :, :2*self.n+1, :, 1] = self.info['ynodes'][np.newaxis, :2*self.n+1, np.newaxis]
-                elif ygrid[p] >= self.info['ny'] - self.n - 1:
-                    field_points[p, :, :2*self.n+1, :, 1] = self.info['ynodes'][np.newaxis, self.info['ny'] - (2*self.n+1):, np.newaxis]
+                elif ygrid[p] < self.ny:
+                    field_points[p, :, :2*self.ny+1, :, 1] = self.info['ynodes'][np.newaxis, :2*self.ny+1, np.newaxis]
+                elif ygrid[p] >= self.info['ny'] - self.ny - 1:
+                    field_points[p, :, :2*self.ny+1, :, 1] = self.info['ynodes'][np.newaxis, self.info['ny'] - (2*self.ny+1):, np.newaxis]
                 else:
-                    field_points[p, :, :, :, 1] = self.info['ynodes'][np.newaxis, ygrid[p]-self.n:ygrid[p]+self.n+2, np.newaxis]
+                    field_points[p, :, :, :, 1] = self.info['ynodes'][np.newaxis, ygrid[p]-self.ny:ygrid[p]+self.ny+2, np.newaxis]
         zgrid = np.floor(points[:, 2] / self.info['dz']).astype(np.int) % self.info['nz']
         zfrac = (points[:, 2] - self.info['znodes'][zgrid])/self.info['dz']
         for p in range(points.shape[0]):
             field_points[p, :, :, :, 2] = (self.info['znodes'][zgrid[p]]
-                    + np.array(list(range(-self.n, self.n+2)))*self.info['dz'])[:, np.newaxis, np.newaxis]
+                    + np.array(list(range(-self.nz, self.nz+2)))*self.info['dz'])[:, np.newaxis, np.newaxis]
         print('computed points where field is needed, now getting values from DB')
         ## I could in principle call getRaw[...] for each point,
         ## but that would mean a lot of calls to the DB,
@@ -164,9 +178,6 @@ class spline_interpolator:
                                    for k in range(len(self.by[ygrid[p]][dorder[o][1]]))]).astype(field_values.dtype)
                     zb[o] = np.array([self.bz[     bzi][dorder[o][2]][k](zfrac[p])
                                    for k in range(len(self.bz[     bzi][dorder[o][2]]))]).astype(field_values.dtype)
-                    #print ['{0:6}'.format(yb[k]) for k in range(2*self.n + 2)]
-                    #print ['{0:6}'.format(field_points[p, 0, k, 0, 1]) for k in range(2*self.n + 2)]
-                    #print ['{0:6}'.format(field_values[p, 0, k, 0, 1]) for k in range(2*self.n + 2)]
                 result[:, p] = np.einsum('okjil,oi,oj,ok->ol', field_values[None, p], xb, yb, zb)
         return result
     def write_coefficients(self):
@@ -175,7 +186,10 @@ class spline_interpolator:
                 text_file = open(
                         (self.info['name']
                         + '_' + coord
-                        + 'spline_m{0}q{1:0>2}_d{2}_coeff.csv'.format(self.m, self.n*2 + 2, order)),
+                        + 'spline_m{0}q{1:0>2}_d{2}_coeff.csv'.format(
+                                                                getattr(self, 'm' + coord),
+                                                                getattr(self, 'n' + coord)*2 + 2,
+                                                                order)),
                         'w')
                 if self.info[coord + 'periodic']:
                     for point in range(len(self.spline[coord].beta[0][order])):
@@ -269,26 +283,31 @@ class spline_interpolator:
         return s.reshape(tuple(list(x.shape[:-1]) + [f.shape[-1]]))
     def write_cfile(
             self,
-            cfile_name = None,  #'spline_m{0}q{1:0>2}'.format(self.m, self.n*2 + 2),
-            base_cname = None): #'m{0}q{1:0>2}'.format(self.m, self.n*2 + 2)):
+            cfile_name = None,
+            base_cname = None):
         if type(cfile_name) == type(None):
             self.cfile_name = (pyJHTDB.lib_folder +
-                          self.info['name'] + '_' +
-                          'spline_m{0}q{1:0>2}'.format(self.m, self.n*2 + 2))
+                          self.info['name'] + '_spline' +
+                          '_xn{0}q{1}'.format(self.mx, self.nx*2 + 2) +
+                          '_yn{0}q{1}'.format(self.my, self.ny*2 + 2) +
+                          '_zn{0}q{1}'.format(self.mz, self.nz*2 + 2))
         else:
             self.cfile_name = cfile_name
         if type(base_cname) == type(None):
-            self.base_cname = 'm{0}q{1:0>2}'.format(self.m, self.n*2 + 2)
+            base_xname = 'xm{0}q{1}'.format(self.mx, self.nx*2 + 2)
+            base_yname = 'ym{0}q{1}'.format(self.my, self.ny*2 + 2)
+            base_zname = 'zm{0}q{1}'.format(self.mz, self.nz*2 + 2)
+            self.base_cname = base_xname + '_' + base_yname + '_' + base_zname
         else:
             self.base_name = base_name
         if os.path.exists(self.cfile_name + '.c'):
             return None
-        def write_interp1D(bname, fname):
-            tmp_txt  = '('
-            for i in range(self.n*2+1):
-                tmp_txt += '\n' + bname[i] + '*' + fname[i] + ' + '
-            tmp_txt += '\n' + bname[self.n*2+1] + '*' + fname[self.n*2+1] + ')'
-            return tmp_txt
+        #def write_interp1D(bname, fname):
+        #    tmp_txt  = '('
+        #    for i in range(self.n*2+1):
+        #        tmp_txt += '\n' + bname[i] + '*' + fname[i] + ' + '
+        #    tmp_txt += '\n' + bname[self.n*2+1] + '*' + fname[self.n*2+1] + ')'
+        #    return tmp_txt
         cfile = open(self.cfile_name + '.c', 'w')
         ### headers
         cfile.write(
@@ -302,7 +321,11 @@ class spline_interpolator:
             cfile.write(
                     self.spline[coord].write_cfunction(
                         cprefix = coord,
-                        csuffix = '_' + self.base_cname)
+                        csuffix = (
+                            '_' + coord +
+                            'm{0}q{1}'.format(
+                                getattr(self, 'm' + coord),
+                                getattr(self, 'n' + coord)*2 + 2)))
                     + '\n')
         ### write 3D interpolation
         src_txt = (
@@ -324,27 +347,33 @@ class spline_interpolator:
                 'int component;\n' +
                 'int i0, i1, i2;\n' +
                 'int xcounter, ycounter, zcounter;\n' +
-                'float bx[{0}], by[{0}], bz[{0}];\n'.format(self.n*2+2) +
-                'int ix[{0}], iy[{0}], iz[{0}];\n'.format(self.n*2+2))
+                'float bx[{0}], by[{1}], bz[{2}];\n'.format(
+                    self.nx*2+2,
+                    self.ny*2+2,
+                    self.nz*2+2) +
+                'int ix[{0}], iy[{1}], iz[{2}];\n'.format(
+                    self.nx*2+2,
+                    self.ny*2+2,
+                    self.nz*2+2))
         # loop over points
         src_txt += 'for (point = 0; point < npoints; point++)\n{\n'
 #        src_txt += 'fprintf(stderr, "inside point loop, point is %d\\n", point);\n'
         # get polynomials
-        src_txt += 'xbeta_' + self.base_cname + '(diff[0], fractions[point*3+0], bx);\n'
+        src_txt += 'xbeta_' + base_xname + '(diff[0], fractions[point*3+0], bx);\n'
         if self.info['yperiodic']:
-            src_txt += 'ybeta_' + self.base_cname + '(diff[1], fractions[point*3+1], by);\n'
+            src_txt += 'ybeta_' + base_yname + '(diff[1], fractions[point*3+1], by);\n'
         else:
-            src_txt += 'ybeta_' + self.base_cname + '(nodes[3*point+1], diff[1], fractions[point*3+1], by);\n'
-        src_txt += 'zbeta_' + self.base_cname + '(diff[2], fractions[point*3+2], bz);\n'
-        src_txt += 'xindices_' + self.base_cname + '(nodes[3*point+0], ix);\n'
-        src_txt += 'yindices_' + self.base_cname + '(nodes[3*point+1], iy);\n'
-        src_txt += 'zindices_' + self.base_cname + '(nodes[3*point+2], iz);\n'
+            src_txt += 'ybeta_' + base_yname + '(nodes[3*point+1], diff[1], fractions[point*3+1], by);\n'
+        src_txt += 'zbeta_' + base_zname + '(diff[2], fractions[point*3+2], bz);\n'
+        src_txt += 'xindices_' + base_xname + '(nodes[3*point+0], ix);\n'
+        src_txt += 'yindices_' + base_yname + '(nodes[3*point+1], iy);\n'
+        src_txt += 'zindices_' + base_zname + '(nodes[3*point+2], iz);\n'
         # loop over components
         src_txt += 'for (component = 0; component < field_components; component++)\n{\n'
 #        src_txt += 'fprintf(stderr, "inside component loop, component is %d\\n", component);\n'
-        bx = ['bx[{0}]'.format(i) for i in range(self.n*2 + 2)]
-        by = ['by[{0}]'.format(i) for i in range(self.n*2 + 2)]
-        bz = ['bz[{0}]'.format(i) for i in range(self.n*2 + 2)]
+        bx = ['bx[{0}]'.format(i) for i in range(self.nx*2 + 2)]
+        by = ['by[{0}]'.format(i) for i in range(self.ny*2 + 2)]
+        bz = ['bz[{0}]'.format(i) for i in range(self.nz*2 + 2)]
         src_txt += (
                 'i0 = nodes[3*point + 0] - field_offset[0];\n' +
                 'i1 = nodes[3*point + 1] - field_offset[1];\n' +
@@ -355,24 +384,24 @@ class spline_interpolator:
                     'fprintf(stderr, "exiting interpolate now, results are most likely nonsensical\\n");\n' +
                     'return EXIT_FAILURE;\n' +
                 '}\n')
-        fzname = []
-        for i in range(self.n*2 + 2):
-            fyname = []
-            for j in range(self.n*2 + 2):
-                fxname = []
-                for k in range(self.n*2 + 2):
-                    fxname.append(
-                           ('field[(((i2+iz[{0}])*field_size[1]' +
-                                 ' + (i1+iy[{1}]))*field_size[2]' +
-                                 ' + (i0+ix[{2}]))*field_components + component]').format(i, j, k))
-                fyname.append(write_interp1D(bx, fxname) + '\n')
-            fzname.append(write_interp1D(by, fyname) + '\n')
+        #fzname = []
+        #for i in range(self.nz*2 + 2):
+        #    fyname = []
+        #    for j in range(self.ny*2 + 2):
+        #        fxname = []
+        #        for k in range(self.nx*2 + 2):
+        #            fxname.append(
+        #                   ('field[(((i2+iz[{0}])*field_size[1]' +
+        #                         ' + (i1+iy[{1}]))*field_size[2]' +
+        #                         ' + (i0+ix[{2}]))*field_components + component]').format(i, j, k))
+        #        fyname.append(write_interp1D(bx, fxname) + '\n')
+        #    fzname.append(write_interp1D(by, fyname) + '\n')
         #src_txt += 'result[field_components*point + component] = ' + write_interp1D(bz, fzname) + ';\n'
         src_txt += (
                 'result[field_components*point + component] = 0;\n' +
-                'for (zcounter = 0; zcounter < {0}; zcounter++)\n'.format(self.n*2+2) +
-                'for (ycounter = 0; ycounter < {0}; ycounter++)\n'.format(self.n*2+2) +
-                'for (xcounter = 0; xcounter < {0}; xcounter++)\n'.format(self.n*2+2) +
+                'for (zcounter = 0; zcounter < {0}; zcounter++)\n'.format(self.nz*2+2) +
+                'for (ycounter = 0; ycounter < {0}; ycounter++)\n'.format(self.ny*2+2) +
+                'for (xcounter = 0; xcounter < {0}; xcounter++)\n'.format(self.nx*2+2) +
                 'result[field_components*point + component] += ' +
                 'bz[zcounter]*by[ycounter]*bx[xcounter]*' +
                 'field[(((i2 + iz[zcounter]) *field_size[1] + ' +
