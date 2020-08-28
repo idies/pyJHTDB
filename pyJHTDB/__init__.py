b8a938cfaea24ce205999140d05b720527d33da4
mzwang2012 <mzwang2012@gmail.com>
2020-08-27 21:30:01 -0400

    Add files via upload
    Version 2020-08-27
    Turblib is updated to use goap-2.8.91, a newer version

--- pyJHTDB/__init__.py
+++ pyJHTDB/__init__.py
@@ -66,7 +66,7 @@ else:
 auth_token = 'edu.jhu.pha.turbulence.testing-201406'
 homefolder = os.path.expanduser('~')
 lib_folder = os.path.join(homefolder, '.config/', 'JHTDB/')
-version_info = "20200725.0"
+version_info = "20200827.0"
 version = str(version_info)
 
 # check if .config/JHTDB folder exists, create it if not
